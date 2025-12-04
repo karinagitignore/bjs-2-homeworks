ecbd11a897aec238452b6a038eb3e74800d2196c
[Karina] <[carinaekuzmicheva@yandex.ru]>
2025-12-04 21:15:48 +0300

    Третье задание полностью готово

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,18 @@
 function compareArrays(arr1, arr2) {
-  
+  if (arr1.length !== arr2.length) return false;
+
+  return arr1.every((value, index) => value === arr2[index]);
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+  const filteredAges = users
+    .filter(user => user.gender === gender)
+    .map(user => user.age);
+
+  if (filteredAges.length === 0) return 0;
+  const sum = filteredAges.reduce((acc, age) => acc + age, 0);
+  const avg = sum / filteredAges.length;
+
+  return avg;
+
 }
